24122794ed6377ca4e57b475c1787e7872e88740
zahra <zahra.admedi2020@gmail.com>
2026-09-06 12:55:40 -0400

    Add Get status API

--- Project-18/app.py
+++ Project-18/app.py
@@ -1,7 +1,14 @@
+from flask import Flask, jsonify
+app=Flask(__name__)
 def status():
     return{
         "status":"online",
         "project":"Delta-500",
         "week":9
     }
-print(status())
+@app.route("/status", methods=["GET"])
+def get_status():
+    return jsonify(status())
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)    
