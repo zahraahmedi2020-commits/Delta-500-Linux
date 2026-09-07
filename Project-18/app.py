db1dcc5afcf7fe64a4e83fc4fbbaffee53b80c45
zahra <zahra.admedi2020@gmail.com>
2026-09-07 12:10:11 -0400

    Add POST API endpoint

--- Project-18/app.py
+++ Project-18/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 app=Flask(__name__)
 def status():
     return{
@@ -6,9 +6,13 @@ def status():
         "project":"Delta-500",
         "week":9
     }
-@app.route("/status", methods=["GET"])
-def get_status():
-    return jsonify(status())
+@app.route("/data", methods=["POST"])
+def receive_data():
+    data = request.json
+    return {
+        "received":data,
+        "status":"success"
+    }
 
 if __name__=="__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)    
