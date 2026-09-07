f0b2cedf5ddcfc0ad0e20a8d3590d32d0e68e54a
zahra <zahra.admedi2020@gmail.com>
2026-09-07 12:45:10 -0400

    Add PUT API endpoint

--- Project-18/app.py
+++ Project-18/app.py
@@ -6,13 +6,15 @@ def status():
         "project":"Delta-500",
         "week":9
     }
-@app.route("/data", methods=["POST"])
-def receive_data():
+
+@app.route("/data/<int:id>", methods=["PUT"])
+def update_data(id):
     data = request.json
     return {
-        "received":data,
-        "status":"success"
-    }
+        "id":id,
+        "updated": data,
+        "status":"updated"
+    } 
 
 if __name__=="__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)    
