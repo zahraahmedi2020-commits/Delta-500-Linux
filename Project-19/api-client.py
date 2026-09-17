25db00c9ea474e55a6e7177bc3623e2c97e17146
zahra <zahra.admedi2020@gmail.com>
2026-09-17 22:07:12 +0330

    complete Python API client practice

--- Project-19/api-client.py
+++ Project-19/api-client.py
@@ -1,60 +1,97 @@
 import requests
 
 url = "http://127.0.0.1:5000/data"
-response = requests.get(url)
 
-print(response.status_code)
-data = response.json()
-items = data["items"]
-online_count = 0
-for item in items:
-    print("Device:", item["name"])
-    print("status:", item["status"])
-    if item["status"] == "online":
-        online_count += 1
-print("online items:", online_count)
-item_exists = False
 
-for item in items:
-    if item["id"] ==3:
-        item_exists = True
-        break
-if not item_exists:
-    new_item = {
-       "id": 3,
-       "name": "Switch",
-       "status": "online"
-    }
+def get_items():
+    response = requests.get(url)
 
-    post_response = requests.post(url, json=new_item)
+    print(response.status_code)
 
-    print(post_response.status_code)
-    print(post_response.json())
+    data = response.json()
+    return data["items"]
 
-    if post_response.status_code == 200:
-       print("Item created successfully")
+def show_items():
+    items = get_items()
+    online_count = 0
+
+    for item in items:
+        print("Device:", item["name"])
+        print("status:", item["status"])
+        if item["status"] == "online":
+           online_count += 1
+    print("online items:", online_count)
+
+
+
+def create_item(item):
+    response = requests.post(url, json=item)
+
+    print(response.status_code)
+    print(response.json())
+
+    if response.status_code == 200:
+        print("Item created successfully")
     else:
-       print("Failed to create item")
-else:
-    print("item Whith ID 3 lready exists")
+        print("Failed to create item")
 
 
-update_item = {
-    "name": "Main Switch",
-    "status": "offline"
-}
-
-put_response = requests.put(
-    "http://127.0.0.1:5000/data/3",
-    json=update_item
-)
-
-print(put_response.status_code)
-print(put_response.json())
 
 
-delete_response = requests.delete(
-    "http://127.0.0.1:5000/data/1",)
 
-print(delete_response.status_code)
-print(delete_response.json())
+def update_item(item_id, item):
+    put_response = requests.put(
+        f"{url}/{item_id}",
+        json=item
+    )
+
+    print(put_response.status_code)
+    print(put_response.json())
+
+def delete_item(item_id):
+    delete_response = requests.delete(
+        f"{url}/{item_id}"
+    )
+
+    print(delete_response.status_code)
+    print(delete_response.json())
+
+while True:
+  choice = input("""
+1. Show Items
+2. Create Item
+3. Update Item
+4. Delete Item
+5. Exist
+
+Enter your choice:
+""")
+  if  choice == "1" :
+     show_items()
+  elif choice == "2" :
+      items = get_items()
+      if items:
+          new_id = max(item["id"] for item in items) +1
+      else:
+         new_id = 1
+      name = input("Enter divice name: ")
+      create_item({
+         "id": new_id,
+         "name": name,
+         "status": "online"
+       })
+  elif choice == "3" :
+      item_id = int(input("Enter item ID to update:"))
+      new_name = input("Enter new device:")
+      update_item(item_id, {
+         "name": new_name,
+         "status": "offline"
+       })
+  elif choice == "4" :
+      item_id = int(input("Enter item ID to delete:"))
+      delete_item(item_id)
+  elif choice == "5" :
+      print("Existing...")
+      break
+  else:
+    print("Invalid chice. Please try agin.")
